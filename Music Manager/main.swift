18a25af99e578c90af97ae0087b4b5b57e1f8648
Will Tyler <will.tyler11@gmail.com>
2018-12-24 23:41:31 +0000

    Setup CLI

---
+++ Music Manager/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Music Manager
+//
+//  Created by Will Tyler on 12/24/18.
+//  Copyright © 2018 Will Tyler. All rights reserved.
+//
+
+import Foundation
+
+
+print("Hello, World!")
